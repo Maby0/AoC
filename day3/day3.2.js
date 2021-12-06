da3464690587cb8ca076cde076f27d5540fa451b
mabon <mabonapgwyn@hotmail.co.uk>
2021-12-06 19:24:03 +0000

    passing tests for CO2 method

--- day3/day3.2.js
+++ day3/day3.2.js
@@ -16,6 +16,20 @@ function oxygenGeneratorRating(array) {
     return filterArr[0]
 }
 
+function CO2ScrubberRating(array) {
+    let i = 0;
+    let filterArr = array;
+    while (filterArr.length !== 1) {
+        let avgBin = epsilonRate(filterArr).split("");
+        filterArr = filterArr.filter(binary => {
+            return binary.charAt(i) === avgBin[i]
+        })
+        i++;
+    }
+    console.log(filterArr);
+    return filterArr[0]
+}
+
 console.log(oxygenGeneratorRating(arr) + " = " + binaryToDecimal(oxygenGeneratorRating(arr)))
 
-module.exports = { oxygenGeneratorRating }
+module.exports = { oxygenGeneratorRating, CO2ScrubberRating }
--- day3/day3.2Spec.js
+++ day3/day3.2Spec.js
@@ -1,10 +1,29 @@
-const { oxygenGeneratorRating } = require('./day3.2')
+const { oxygenGeneratorRating, CO2ScrubberRating } = require('./day3.2')
 
 describe('oxygenGeneratorRating', () => {
     let binArr;
 
-    it('returns values with 0 in right position if equally common', () => {
+    it('returns one binary number after filtering for most common bit', () => {
         binArr = ["10", "11", "01"]
         expect(oxygenGeneratorRating(binArr)).toEqual("11")
+    })
+
+    it('returns one binary number after filtering for most common bit', () => {
+        binArr = ["100", "110", "111", "101", "010"]
+        expect(oxygenGeneratorRating(binArr)).toEqual("111")
+    })
+})
+
+describe('CO2ScrubberRating', () => {
+    let binArr;
+
+    it('returns one binary number after filtering for least common bit', () => {
+        binArr = ["10", "11", "01"]
+        expect(CO2ScrubberRating(binArr)).toEqual("01")
+    })
+
+    it('returns one binary number after filtering for least common bit', () => {
+        binArr = ["100", "110", "111", "101", "010"]
+        expect(CO2ScrubberRating(binArr)).toEqual("010")
     })
 })
