2e67f20651c71f0772912f3c63189f21a4768a7c
mabon <mabonapgwyn@hotmail.co.uk>
2021-12-06 15:31:40 +0000

    challenge 3.1 completed

--- day3/day3.1.js
+++ day3/day3.1.js
@@ -15,20 +15,52 @@ function gammaRate(array) {
             i++;
         })
     });
-    return _checkPosNeg(binarr)
+    return _checkPosNegGamma(binarr)
+}
+
+function epsilonRate(array) {
+    let binarr = []
+    for (var n = 1; n <= array[0].length; n++) {
+        binarr.push(0);
+    }
+    let i;
+    array.forEach(binary => {
+        i = 0;
+        binary.split("").forEach(n =>{
+            if (n === "0") binarr[i] --;
+            if (n === "1") binarr[i] ++;
+            i++;
+        })
+    });
+    return _checkPosNegEpsilon(binarr)
 }
 
 function binaryToDecimal(binStr) {
     return parseInt(binStr, 2)
 }
 
-function _checkPosNeg(array) {
+function _checkPosNegGamma(array) {
     return array.map(n => {
         if (parseInt(n) > 0) return "1"
         if (parseInt(n) < 0) return "0"
     }).join("")
 }
 
+function _checkPosNegEpsilon(array) {
+    return array.map(n => {
+        if (parseInt(n) > 0) return "0"
+        if (parseInt(n) < 0) return "1"
+    }).join("")
+}
+
+let gammaRateCalc = gammaRate(arr)
+let epsilonRateCalc = epsilonRate(arr)
+
+console.log(gammaRateCalc)
 console.log(binaryToDecimal(gammaRate(arr)))
+console.log(epsilonRateCalc)
+console.log(binaryToDecimal(epsilonRate(arr)))
+
+console.log("FINAL ANSWER ==== " + binaryToDecimal(gammaRateCalc) * binaryToDecimal(epsilonRateCalc))
 
 module.exports = { gammaRate, binaryToDecimal }
