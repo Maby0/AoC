012e9b0ceabc7ef90fb6d2874842c7b03508560a
mabon <mabonapgwyn@hotmail.co.uk>
2021-12-12 13:53:15 +0000

    additional tests for check methods

--- day4/day4.1Spec.js
+++ day4/day4.1Spec.js
@@ -42,6 +42,16 @@ describe('checkIfRow', () => {
         expect(checkIfRow(boards)).toEqual(true)
     })
 
+    it('returns true if a row is all ints', () => {
+        let boards = [
+            [
+                ["1", "2"],
+                [3, 4]
+            ]
+        ]
+        expect(checkIfRow(boards)).toEqual(true)
+    })
+
     it('returns false if no row is all ints', () => {
         let boards = [
             [
@@ -65,6 +75,16 @@ describe('checkIfColumn', () => {
     })
 
     it('returns true if a column is all ints', () => {
+        let boards = [
+            [
+                ["1", 2],
+                ["3", 4]
+            ]
+        ]
+        expect(checkIfColumn(boards)).toEqual(true)
+    })
+
+    it('returns false if no column is all ints', () => {
         let boards = [
             [
                 [1, "2"],
